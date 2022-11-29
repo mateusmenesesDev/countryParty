f2b7de7cdf2d36be68e9cdcd3673403d2f7dccc9
Paulo Victor Souza <p.victordsouza@gmail.com>
2022-11-29 01:30:22 -0300

    começando dark mode falta corrigir hover

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const darkMode = document.querySelector('#dark-mode');
+window.onload = () => {
+  const darkMode = document.querySelector('#dark-mode-container');  
+  darkMode.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('dark-mode-background');
+    document.querySelector('header').classList.toggle('dark-mode-elements');
+    const elements = document.querySelectorAll('.elements');
+    for (const element of elements) {
+      element.classList.toggle('dark-mode-elements');      
+    }
+  })
 
-darkMode.addEventListener('click', () => {
-  document.querySelector('body').classList.toggle('dark-mode');
-})
+};
+
