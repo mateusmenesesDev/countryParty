94d897facc139dc17234c20f3ba092e033396516
Paulo Victor Souza <p.victordsouza@gmail.com>
2022-11-29 22:39:15 -0300

    corrigindo espaços em branco

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 window.onload = () => {
-  const darkMode = document.querySelector('#dark-mode-container');  
+  const darkMode = document.querySelector('#dark-mode-container');
   darkMode.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('dark-mode-background');
     document.querySelector('header').classList.toggle('dark-mode-elements');
     const elements = document.querySelectorAll('.elements');
     for (const element of elements) {
-      element.classList.toggle('dark-mode-elements');      
+      element.classList.toggle('dark-mode-elements');
     }
   })
-
 };
-
